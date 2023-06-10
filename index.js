b041202e693381a4fc9d1bb15abef6f324e2d42c
giomendes.quezada <gioq89@gmail.com>
2023-06-10 16:04:12 -0700

    updated inquirer package and corrected error with finding shapes module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { Triangle, Circle, Square } = require("./shapes");
+const { Triangle, Circle, Square } = require("./lib/shapes");
 
 async function generateLogo() {
   const answers = await inquirer.prompt([
